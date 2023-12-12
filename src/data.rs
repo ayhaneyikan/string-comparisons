e638ed2a752e35bb02204976fee98843cbac6f4f
Ayhan Eyikan <aeyikan@umd.edu>
2023-12-12 18:51:45 -0500

    separating common data needs

---
+++ src/data.rs
@@ -0,0 +1,35 @@
+use serde::Serialize;
+
+/// Reads dataset from csv and returns vector of the response fields
+pub fn read_dataset() -> Vec<String> {
+    let mut data = vec![];
+    let mut reader = csv::Reader::from_path("dataset.csv").unwrap();
+    for r in reader.records() {
+        let record = r.unwrap();
+        data.push(
+            record
+                .get(1)
+                .unwrap()
+                .trim_matches(|c| c == '\'' || c == '\"')
+                .to_string(),
+        );
+    }
+    data
+}
+
+#[derive(Serialize)]
+pub struct FullDistanceComparison {
+    pub i: usize,
+    pub j: usize,
+    pub lev: usize,
+    pub jaro: f64,
+    pub jac: f64,
+    pub dice: f64,
+}
+
+#[derive(Serialize)]
+pub struct LevComparison {
+    pub i: usize,
+    pub j: usize,
+    pub norm_lev: f64,
+}
