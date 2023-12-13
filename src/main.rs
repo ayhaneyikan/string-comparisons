dc1c7e884fe3e1120d82545c721a35ba68a6c33a
Ayhan Eyikan <aeyikan@umd.edu>
2023-12-12 19:19:24 -0500

    updating commandline parsing logic

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,25 @@ use std::env;
 use procedural::procedural;
 use thread::threaded;
 
-mod thread;
-mod procedural;
 mod data;
+mod procedural;
+mod thread;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
-    match args[1].as_str() {
-        "-p" => procedural(),
-        "-t full" => threaded(thread::Payload::Full),
-        "-t lev" => threaded(thread::Payload::LevOnly),
-        _ => println!("Usage -p : procedural\n-t full : threaded full comparison\n-t lev : threaded normalized levenshtein comparison only"),
+
+    if args.len() == 2 && args[1] == "-p" {
+        procedural();
+    } else if args.len() == 3 {
+        if args[2] == "full" {
+            threaded(thread::Payload::Full);
+            return;
+        }
+        if args[2] == "lev" {
+            threaded(thread::Payload::LevOnly);
+            return;
+        }
     }
+
+    println!("Usage\t-p\t: procedural\n\t-t full\t: threaded full comparison\n\t-t lev\t: threaded normalized levenshtein comparison only");
 }
